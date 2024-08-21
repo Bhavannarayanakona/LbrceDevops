f3127454fccc0a1f13398b0d76e49725843cb37f
AdmiN <AdmiN@ITWS0021>
2024-08-21 15:13:05 +0530

    commit

---
+++ src/com/lbrce/login.java
@@ -0,0 +1,24 @@
+package com.lbrce;
+
+import java.util.Scanner;
+
+public class login {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 String correctUsername = "user";
+	     String correctPassword = "password";
+	     Scanner scanner = new Scanner(System.in);
+	     System.out.print("Enter your Username: ");
+	     String username = scanner.nextLine();
+	     System.out.print("Enter your Password: ");
+	     String password = scanner.nextLine();
+	     if (username.equals(correctUsername) && password.equals(correctPassword)) {
+	         System.out.println("Login successful!");
+	     } else {
+	         System.out.println("Invalid Username or Password. Please try again.");
+	     }
+	     scanner.close();
+	}
+
+}
