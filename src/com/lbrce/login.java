8036d87deab8577f880ca8a14e12a4d8a8814e34
AdmiN <AdmiN@ITWS0021>
2024-08-21 15:16:37 +0530

    commit

--- src/com/lbrce/login.java
+++ src/com/lbrce/login.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class login {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		 String correctUsername = "user";
 	     String correctPassword = "password";
 	     Scanner scanner = new Scanner(System.in);
